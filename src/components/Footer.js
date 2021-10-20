2acbd928a35d4394e9f626ebc82a925da3fd22b3
Brad R <brad.w.rollinson@gmail.com>
2021-10-20 16:05:36 -0400

    add footer images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import github from '../assets/images/github.png';
+import linkedin from '../assets/images/linkedin.png';
+import email from '../assets/images/email.png';
+
 const styles = {
+    footer: {
+        padding: '20px',
+        position: 'fixed',
+        bottom: '0',
+        height: '75px',
+        display: 'flex',
+        justifyContent: 'center'
+    },
     github: {
-        width: '75px'
+        height: '100%'
+    },
+    linkedin: {
+        height: '100%'
+    },
+    email: {
+        height: '95%',
+        marginLeft: '5px'
     }
 }
 
 function Footer() {
     return (
-        <div>
+        <div style={styles.footer}>
             <img src={github} alt="github logo" style={styles.github} />
+            <img src={linkedin} alt="github logo" style={styles.linkedin} />
+            <img src={email} alt="github logo" style={styles.email} />
         </div>
     )
 }
